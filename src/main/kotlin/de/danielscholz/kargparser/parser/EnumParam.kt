2075e1914041ba81b6ad03527fc43ff0a9d0e111
Daniel Scholz <danielsch@gmx.net>
2020-05-23 17:05:50 +0200

    added support for enum classes

---
+++ src/main/kotlin/de/danielscholz/kargparser/parser/EnumParam.kt
@@ -0,0 +1,35 @@
+package de.danielscholz.kargparser.parser
+
+import de.danielscholz.kargparser.ArgParseException
+
+class EnumParam<T : Enum<T>>(private val enumType: T, private val typeDescription: String? = null) : ParamParserBase<T, T?>() {
+
+   override var callback: ((T) -> Unit)? = null
+   private var value: T? = null
+
+   override fun matches(rawValue: String): Boolean {
+      for (enumConstant in enumType.declaringClass.enumConstants) {
+         if (enumConstant.name.equals(rawValue, true)) {
+            return true
+         }
+      }
+      return false
+   }
+
+   override fun assign(rawValue: String) {
+      for (enumConstant in enumType.declaringClass.enumConstants) {
+         if (enumConstant.name.equals(rawValue, true)) {
+            value = enumConstant
+            return
+         }
+      }
+   }
+
+   override fun exec() {
+      callback?.invoke(value!!) ?: throw ArgParseException("callback must be specified!", argParser!!)
+   }
+
+   override fun printout(): String {
+      return typeDescription ?: "value"
+   }
+}
